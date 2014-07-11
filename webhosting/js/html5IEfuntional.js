067fe847e6fe3dee0a20c606bd39ac197f66d1c8
James Arthur Phillips I <mrjap1jobs@gmail.com>
2014-07-11 13:05:58 -0400

    Code Samples Of Html5, Css3, JS, Php, Design
    Code Samples for Anyone is Question of my Design/Coding Skills

---
+++ webhosting/js/html5IEfuntional.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+/* 
+ * Create HTML5 elements for IE's sake
+ * Reference: http://ejohn.org/blog/html5-shiv/
+ * Reference: http://remysharp.com/2009/01/07/html5-enabling-script/
+ */
+
+
+ for(var e,l='article aside figcaption figure footer header hgroup nav section time'.split(' ');e=l.pop();document.createElement(e))
+
+ 
+
+	document.createElement('article');
+	document.createElement('caption');
+	document.createElement('footer');
+	document.createElement('header');
+	document.createElement('section');
+	document.createElement('aside');
+	document.createElement('hgroup');
+	document.createElement('video');
+	document.createElement('nav');
+
+
